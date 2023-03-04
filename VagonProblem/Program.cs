797c7f0d251d4de2a9ee9d7df4da213106972c1a
Bahadir <Bahadir@DESKTOP-GETKPVS>
2023-03-04 15:04:40 +0300

    Add project files.

---
+++ VagonProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VagonProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gelenVagonlar = new string[] { "10808313931813319430761116496", "93876532983858416774152932536" };
+            var depoDurum = "0#54134427902231984111412732221";
+            VagonProblem.AntrepoYerlestir(depoDurum, gelenVagonlar);
+        }
+    }
+}
---
+++ VagonProblem/VagonProblem.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+namespace VagonProblem
+{
+    public class VagonProblem
+    {
+        public static string AntrepoYerlestir(string depoDurum, string[] gelenVagonlar)
+        {
+            // Parametre olarak gönderilen depoların ve gelen vagonların sayısının "tam sayı sınırını aşmayacağı" varsayıldı.
+            var depoKapasitesi = 9;
+            var depolar = depoDurum.Replace("#", "").Select(x => int.Parse(x.ToString())).ToArray();
+
+            foreach (var vagon in gelenVagonlar)
+            {
+                var vagonDizisi = vagon.Select(x => int.Parse(x.ToString())).ToArray();
+
+                for (var vagonIndeks = vagonDizisi.Length - 1; vagonIndeks >= 0; vagonIndeks--)
+                {
+                    if (vagonDizisi[vagonIndeks] > 0)
+                    {
+                        var depoIndeks = vagonIndeks + 1;
+                        while (depoIndeks >= 0)
+                        {
+                            var ilkNokta = depoIndeks;
+                            vagonDizisi[vagonIndeks]--;
+                            while (depolar[depoIndeks] == depoKapasitesi)
+                            {
+                                depolar[depoIndeks] = 0;
+                                depoIndeks--;
+                            }
+                            depolar[depoIndeks]++;
+                            depoIndeks = ilkNokta;
+                            if (vagonDizisi[vagonIndeks] == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return string.Join("", depolar).Insert(1, "#");
+        }
+    }
+}
+
+
